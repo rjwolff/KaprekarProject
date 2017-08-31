5ca8a004cc8ec2b33eff75de2b4bc9ac5d532ba2
Ryan Wolff <bigboi@pcp352853pcs.unl.edu>
2017-08-30 22:20:59 -0500

    I FIXED A LOGIC ERROR
    THE STUPID N * N NEEDED TO BE POW(N,2) WHY? IDK WHO KNOWS, KARL DIDN'T KNOW. ALSO I CHANGED THE LOG TO FINDING THE DIGITS OF SQUARE NOT N, AND SECOND AS GREATER THAN 0, NOT FIRST

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -29,16 +29,16 @@ public class KaprekarUtils {
 		} 
 		
 		//we use a long to accommodate larger squares
-		long square = n * n;
-		int numDigits = (int) Math.log10(n) + 1;
+		long square = (long) Math.pow(n, 2);
+		int numDigits = (int) Math.log10(square) + 1;
 		long modulus = 1;
 		long first, second;
 		for(int i=1; i<=numDigits; i++) {
 			modulus *= 10;
 			first = square / modulus;
 			second = square % modulus;
-			if(first > 0 &&
-			   first + second == n) {
+			if(first + second == n &&
+				second > 0) {
 				return true;
 			}
 		}
